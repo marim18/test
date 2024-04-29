791edf04536bc86d390c39f828d900dd3256ecd3
Maria Møll <marim18@uia.no>
2024-04-29 16:11:28 +0200

    Fixed button issue and win checker

--- Program.cs
+++ Program.cs
@@ -28,24 +28,37 @@ namespace fitnessgame
             s_GpioController = new GpioController();
             GpioPin BluePin = s_GpioController.OpenPin(13, PinMode.Output);
             GpioPin BlueButton = s_GpioController.OpenPin(14, PinMode.InputPullUp);
-           // GpioPin examplePin = s_GpioController.OpenPin(13, PinMode.Output);
+            // GpioPin examplePin = s_GpioController.OpenPin(13, PinMode.Output);
             //GpioPin exampleButton = s_GpioController.OpenPin(14, PinMode.InputPullUp);
             GpioPin redpin = s_GpioController.OpenPin(32, PinMode.Output);
             GpioPin redButton = s_GpioController.OpenPin(16, PinMode.InputPullUp);
-            GpioPin yellowPin = s_GpioController.OpenPin(27, PinMode.Output);
-            GpioPin yellowButton = s_GpioController.OpenPin(26, PinMode.InputPullUp);
+            GpioPin yellowPin = s_GpioController.OpenPin(26, PinMode.Output);
+            GpioPin yellowButton = s_GpioController.OpenPin(27, PinMode.InputPullUp);
             GpioPin greenPin = s_GpioController.OpenPin(25, PinMode.Output);
             GpioPin greenButton = s_GpioController.OpenPin(33, PinMode.InputPullUp);
-            PinButtonPair Redpair = new PinButtonPair(redpin,redButton);
+            PinButtonPair Redpair = new PinButtonPair(redpin, redButton);
             PinButtonPair Bluepair = new PinButtonPair(BluePin, BlueButton);
             PinButtonPair yellowpair = new PinButtonPair(yellowPin, yellowButton);
             PinButtonPair greenpair = new PinButtonPair(greenPin, greenButton);
-            PinButtonPair[] ListofPairs = new PinButtonPair[4]{ Redpair,Bluepair,yellowpair,greenpair};
+            PinButtonPair[] ListofPairs = new PinButtonPair[4] { Redpair, Bluepair, yellowpair, greenpair };
 
             while (true)
             {
+                foreach (PinButtonPair pair in ListofPairs)
+                {
+                    pair.Pin.Write(PinValue.Low);
+                   
+                }
+                Console.WriteLine("start setup");
                 GpioPin[] newsequence = Sequence(ListofPairs);
+
                 inputmode(newsequence);
+                foreach (PinButtonPair pair in ListofPairs)
+                {
+                    pair.Pin.Write(PinValue.Low);
+                  
+                }
+                Console.WriteLine("end setup");
             }
             GpioPin[] Sequence(PinButtonPair[] pinButtonPairs)
             {
@@ -60,15 +73,29 @@ namespace fitnessgame
                 }
 
                 // Play the sequence
-                foreach (int index in sequence)
+                
+                for (int i = 0; i < sequence.Length; i++)
                 {
-                    pinButtonPairs[index].Pin.Write(PinValue.High);
+                    pinButtonPairs[sequence[i]].Pin.Write(PinValue.High); // Light up the LED
                     Thread.Sleep(500); // Wait for half a second
-                    Randomsequence[index] = pinButtonPairs[index].Pin;
-                    pinButtonPairs[index].Pin.Write(PinValue.Low);
+                    Randomsequence[i] = pinButtonPairs[sequence[i]].Pin; // Save the pin for later comparison
+                    pinButtonPairs[sequence[i]].Pin.Write(PinValue.Low);  // Turn of the LED
                     Thread.Sleep(200); // Wait for 200 milliseconds between LEDs
                 }
-                return Randomsequence;//needs to return gpio[] for compare function
+                
+                return Randomsequence; //needs to return gpio[] for compare function
+            }
+             bool winnerchecker(GpioPin[] sequence, GpioPin[] sequencelist)
+            {
+                for (int i = 0; i < sequence.Length; i++)
+                {
+                    if (sequence[i] != sequencelist[i])
+                    {
+                        Console.WriteLine();
+                        return false;
+                    }
+                }
+                return true;
             }
             void inputmode(GpioPin[] sequence)
             {
@@ -76,50 +103,67 @@ namespace fitnessgame
                 bool playing = true;
                 while (playing)
                 {
-                    GpioPin[]sequencelist = new GpioPin[4];
-                    for (int i = 0; i < sequence.Length; )
+                    GpioPin[] sequencelist = new GpioPin[4];
+                    for (int i = 0; i < sequence.Length; i++)
                     {
-                        foreach (PinButtonPair pair in ListofPairs)
+                        bool pressed_button = false;
+                        while (!pressed_button)
                         {
-                            if (pair.Button.Read() == PinValue.Low)
+                            foreach (PinButtonPair pair in ListofPairs)
                             {
-                                pair.Pin.Toggle();
-                                sequencelist[i] = pair.Pin;
-                                Thread.Sleep(123);
-                                i++;
-                            }
-                            else
-                            {
-                                pair.Pin.Write(PinValue.Low);
-                            }
+                                if (pair.Button.Read() == PinValue.Low)
+                                {
 
+                                    pair.Pin.Write(PinValue.High);
+                                    sequencelist[i] = pair.Pin;
+                                    Thread.Sleep(100);
+                                    pair.Pin.Write(PinValue.Low);
+                                    pressed_button = true;
+                                }
+                                else
+                                {
+                                    pair.Pin.Write(PinValue.Low);
+                                }
+                            }
                         }
-                    }
-                    for (int i = 0; i < sequence.Length;)
-                    {
-                        if (i == sequence.Length-1 && sequence[i] == sequencelist[i])
+                        bool released_buttons = false;
+                        while (!released_buttons)
                         {
-                            Console.WriteLine("You win");
+                            bool is_button_down = false;
+                            foreach (PinButtonPair pair in ListofPairs)
+                            {
+                                if (pair.Button.Read() == PinValue.Low)
+                                {
+                                    is_button_down = true;
+                                }
+                            }
+                            Thread.Sleep(100);
+                            if (!is_button_down)
+                            { break; }
+                        }
+                        if (winnerchecker(sequence, sequencelist))
+                        {
+                            Console.WriteLine("You win!");
+                            foreach (PinButtonPair pair in ListofPairs)
+                            {
+                                pair.Pin.Write(PinValue.High);
+                            }
                             playing = false;
-                        }
-                        else if(i != sequence.Length && sequence[i] == sequencelist[i])
-                        {
-                            i++;
+                            Thread.Sleep(1000);
                         }
                         else
                         {
                             Console.WriteLine("you lose");
                             playing = false;
-                            break;
-                            
                         }
-                    };
-                    
+                    }
+                  
+
 
                 }
             }
 
         }
-       
+
     }
 }
